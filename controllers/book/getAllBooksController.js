84b45ae8620d580cf4b683ad4a8dcc562ea49a39
Amaldeep <amaldeeppatra21@gmail.com>
2025-04-09 03:30:34 +0530

    added filter and search feature

--- controllers/book/getAllBooksController.js
+++ controllers/book/getAllBooksController.js
@@ -1,11 +1,33 @@
 const Book = require('../../models/book');
+const { Decimal128 } = require('mongodb');
 
 async function getAllBooksHandler(req, res){
     try{
-        const books = await Book.find({});
+        const { author, category, rating, title } = req.query;
+
+        // for filtering by author, category, and rating
+        const filter = {};
+        if (author) {
+            filter.author = author;
+        }
+        if (category) {
+            filter.category = category;
+        }
+        if (rating) {
+            filter.rating = Decimal128.fromString(rating.toString());
+        }
+
+        // for searching by title - partial search
+        if (title) {
+            filter.title = { $regex: title, $options: 'i' };
+        }
+
+        const books = await Book.find(filter);
+
         if(!books || books.length === 0){
             return res.status(404).json({message: "No books found"});
         }
+        
         return res.status(200).json({ success: true, books });
     }
     catch (error){
