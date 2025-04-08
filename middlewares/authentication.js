7b676164b544e551cf50fd6ae788171f36477396
Amaldeep <amaldeeppatra21@gmail.com>
2025-04-09 04:31:33 +0530

    created auth middleware for protected routes

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -4,7 +4,7 @@ function ensureAuthenticated(req, res, next) {
   const authHeader = req.headers.authorization;
 
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ message: 'Unauthorized: No token provided' });
+    return res.status(401).json({ message: 'Unauthorized access!!' });
   }
 
   const token = authHeader.split(' ')[1];
@@ -14,7 +14,7 @@ function ensureAuthenticated(req, res, next) {
     req.user = decoded;
     next();
   } catch (err) {
-    return res.status(401).json({ message: 'Unauthorized: Invalid token' });
+    return res.status(401).json({ message: 'Unauthorized - Invalid token' });
   }
 }
 
