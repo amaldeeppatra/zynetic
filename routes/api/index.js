a209ab87948d925ba1bbcd5a10f308342d590162
Amaldeep <amaldeeppatra21@gmail.com>
2025-04-09 04:28:07 +0530

    created auth middleware for protected routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const authMiddleware = require('../../middlewares/authentication');
 
 router.use('/auth', require('./auth/authRoutes'));
 
-router.use('/book', require('./book/bookRoutes'));
+router.use('/book', authMiddleware, require('./book/bookRoutes'));
 
 module.exports = router;
